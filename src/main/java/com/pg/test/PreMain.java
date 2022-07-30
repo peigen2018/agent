a1cd740236628fcbd60046dce0d253c9ba933bfa
peigen <peigen2018@aliyun.com>
2022-07-31 01:25:52 +0800

    backup the code

--- src/main/java/com/pg/test/PreMain.java
+++ src/main/java/com/pg/test/PreMain.java
@@ -6,10 +6,12 @@ import java.lang.instrument.Instrumentation;
 public class PreMain {
     public static void premain(String args, Instrumentation inst) {
 
-        System.out.println("hello world");
+        System.out.println("start agent");
+
+        inst.addTransformer(new ClassFindAll());
     }
 
     public static void premain(String agentArgs) {
-        System.out.println("------ premain方法，有一个入参 ------ agentArgs:" + agentArgs);
+        System.out.println("------------ agentArgs:" + agentArgs);
     }
 }
